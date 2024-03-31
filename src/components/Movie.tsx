72c450e2c9f9b551e5ddabaaa235cc7945551eea
PavelBerikov <NoooB199505@gmail.com>
2024-03-31 13:29:35 +0300

    rating

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -4,10 +4,14 @@ interface IProps{
     movie: IMovie
 }
 const Movie: FC<IProps> = ({movie}) => {
-    const {id, title} = movie;
+    const {id, title, vote_average, vote_count} = movie;
     return (
         <div>
             <div>{id} - {title}</div>
+            <div>average -  {vote_average}</div>
+            <div>count - {vote_count}</div>
+            <button>addRating</button>
+            <hr/>
         </div>
     );
 };
