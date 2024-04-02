4c1eb773bb2c702dceb4b0974c31d7f818dd02d9
PavelBerikov <NoooB199505@gmail.com>
2024-04-02 19:59:06 +0300

    1st

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Route, Routes} from "react-router-dom";
 import {MainLayout} from "./layouts";
+import {Header} from "./components/header/Header";
 
 const App = () => {
     return (
         <div style={{display: "flex"}}>
+            <Header/>
             <Routes>
             <Route path={'/'} element={<MainLayout/>}/>
         </Routes>
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import css from './header.module.css'
+const Header = () => {
+    return (
+        <div className={css.Header}>
+            Header
+        </div>
+    );
+};
+
+export {Header};
